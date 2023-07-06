8f5f5c769b6a5f12a267c7ef6155c43bc0446734
raymondborel <rborel852@gmail.com>
2023-07-05 18:10:25 -0700

    Fixing static setting

--- cars_project/settings.py
+++ cars_project/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main_app/static/')]
 if not DEBUG:
-    STATIC_ROOT = [os.path.join(BASE_DIR, "main_app/static/")]
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
